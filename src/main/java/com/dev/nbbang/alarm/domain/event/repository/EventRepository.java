88eadc7c2c815ee24e8a186cdcc5d8e5eda348a9
zayson <jymaeng95@gmail.com>
2022-06-04 17:22:55 +0900

    feat: 이벤트 리스트 조회 페이징 추가 - [#1]

--- src/main/java/com/dev/nbbang/alarm/domain/event/repository/EventRepository.java
+++ src/main/java/com/dev/nbbang/alarm/domain/event/repository/EventRepository.java
@@ -1,8 +1,12 @@
 package com.dev.nbbang.alarm.domain.event.repository;
 
 import com.dev.nbbang.alarm.domain.event.entity.Event;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
@@ -11,4 +15,6 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     Event save(Event event);
 
     void deleteByEventId(Long eventId);
+
+    Slice<Event> findEventByEventIdLessThanOrderByEventId(Long eventId, Pageable pageable);
 }
