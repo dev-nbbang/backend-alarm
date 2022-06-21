309ba434cae1396cebbec83add240ee330db5b23
zayson <jymaeng95@gmail.com>
2022-06-21 16:22:58 +0900

    test: 알림 API 서비스 Persistence Layer 테스트 - [#7]

--- src/main/java/com/dev/nbbang/alarm/domain/notify/repository/NotifyRepository.java
+++ src/main/java/com/dev/nbbang/alarm/domain/notify/repository/NotifyRepository.java
@@ -2,6 +2,7 @@ package com.dev.nbbang.alarm.domain.notify.repository;
 
 import com.dev.nbbang.alarm.domain.notify.entity.Notify;
 import com.dev.nbbang.alarm.domain.notify.entity.NotifyType;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -17,24 +18,24 @@ public interface NotifyRepository extends JpaRepository<Notify, Long> {
     // 특정 알림 삭제
     void deleteNotifyByNotifyId(Long notifyId);
 
-    // 나에게 온 알림 전체 삭제
+    // 나에게 온 알림 전체 삭제 (all로 오는건 안지워지는데 어케하지..?)
     void deleteAllByNotifyReceiver(String notifyReceiver);
 
     // 알림 타입별 리스트 조회 (필터링, 페이징)
-    @Query("SELECT n FROM Notify n WHERE n.notifyType = :notifyType AND n.notifyReceiver IN ('all', :notifyReceiver)")
-    Slice<Notify> findNotifiesWithFilter(@Param("notifyType") NotifyType notifyType, String notifyReceiver);
+    @Query("SELECT n FROM Notify n WHERE n.notifyType = :notifyType AND n.notifyReceiver IN ('all', :notifyReceiver) ORDER BY n.notifyYmd DESC")
+    Slice<Notify> findNotifiesWithFilter(@Param("notifyType") NotifyType notifyType, String notifyReceiver, Pageable pageable);
 
     // 읽지 않은 알림 리스트 조회 (필터링 , 페이징)
-    @Query("SELECT n FROM Notify n WHERE n.notifyType = :notifyType AND n.notifyReceiver IN ('all', :notifyReceiver) AND n.readYn = 'N'")
-    Slice<Notify> findUnreadNotifiesWithFilter(@Param("notifyType") NotifyType notifyType, String notifyReceiver);
+    @Query("SELECT n FROM Notify n WHERE n.notifyType = :notifyType AND n.notifyReceiver IN ('all', :notifyReceiver) AND n.readYn = 'N' ORDER BY n.notifyYmd DESC")
+    Slice<Notify> findUnreadNotifiesWithFilter(@Param("notifyType") NotifyType notifyType, String notifyReceiver, Pageable pageable);
 
     // 알림 리스트 조회 (알림 전체 조회 , 페이징)
-    @Query("SELECT n FROM Notify n WHERE n.notifyReceiver IN ('all', :notifyReceiver)")
-    Slice<Notify> findNotifies(@Param("notifyReceiver") String notifyReceiver);
+    @Query("SELECT n FROM Notify n WHERE n.notifyReceiver IN ('all', :notifyReceiver) ORDER BY n.notifyYmd DESC")
+    Slice<Notify> findNotifies(@Param("notifyReceiver") String notifyReceiver, Pageable pageable);
 
     // 읽지 않은 알림 리스트 조회 (알림 전체 조회 페이징)
-    @Query("SELECT n FROM Notify n WHERE n.notifyReceiver IN ('all', :notifyReceiver) AND n.readYn = 'N'")
-    Slice<Notify> findUnreadNotifies(@Param("notifyReceiver") String notifyReceiver);
+    @Query("SELECT n FROM Notify n WHERE n.notifyReceiver IN ('all', :notifyReceiver) AND n.readYn = 'N' ORDER BY n.notifyYmd DESC")
+    Slice<Notify> findUnreadNotifies(@Param("notifyReceiver") String notifyReceiver, Pageable pageable);
 
     // 알림 메세지 저장
     Notify save(Notify notify);
