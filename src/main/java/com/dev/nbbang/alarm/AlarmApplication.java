5ad69c2d37e5757ff654c5b614f1c6a58ab07576
zayson <jymaeng95@gmail.com>
2022-07-11 14:59:57 +0900

    setting: Eureka, Cloud Config Client 설정

--- src/main/java/com/dev/nbbang/alarm/AlarmApplication.java
+++ src/main/java/com/dev/nbbang/alarm/AlarmApplication.java
@@ -2,8 +2,10 @@ package com.dev.nbbang.alarm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AlarmApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/dev/nbbang/alarm/global/config/KafkaConsumerConfig.java
+++ src/main/java/com/dev/nbbang/alarm/global/config/KafkaConsumerConfig.java
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -19,6 +20,7 @@ import java.util.Map;
 
 @Configuration
 @EnableKafka
+@RefreshScope
 public class KafkaConsumerConfig {
     private final String host;
     private final String port;
